43ef055704b5c7f4d2e2e787243d122c9fab0be3
sviatoslav-poliakov <Usavaq3773>
2019-12-03 13:24:49 +0200

    [FRONT] singleProduct component

---
+++ resources/js/views/SingleProduct.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 offset-md-2">
+                <img :alt="product.name" :src="product.image">
+                <h3 class="title" v-html="product.name"></h3>
+                <p class="text-muted">{{product.description}}</p>
+                <h4>
+                    <span class="small-text text-muted fload-left">$ {{ product.title }}</span>
+                    <span class="small-text fload-right">Available Quantity {{ product.units }}</span>
+                </h4>
+                <hr>
+                <router-link :to="{ path: '/checkout?pid='+product.id }" class="col-md-4 btn-primary float-right">Buy
+                    Now
+                </router-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data () {
+            return {
+                product: []
+            }
+        },
+        beforeMount () {
+            let url = `/api/products/${this.$route.params.id}`
+            axios.get(url).then(response => this.product = response.data)
+        }
+    }
+</script>
+
+<style scoped>
+    .small-text {
+        font-size: 18px;
+    }
+
+    .title {
+        font-size: 36px;
+    }
+</style>
